cf5f86be28c3e55fc5b289aa3065282157ef3615
sina <sinabaradaran98@gmail.com>
2023-10-31 20:03:14 +0330

    d1

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import "./global.css";
+import { Routes, Route } from "react-router-dom";
+import "./globals.css";
+import { Home } from "./_root/pages";
+import SignInForm from "./_auth/forms/SignInForm";
+import SignUpForm from "./_auth/forms/SignUpForm";
+import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
 
 const App = () => {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-    </div>
+    <main className="flex h-screen">
+      <Routes>
+        {/*public routes*/}
+        <Route element={<AuthLayout />}>
+          <Route path="/sign-in" element={<SignInForm />} />
+          <Route path="/sign-up" element={<SignUpForm />} />
+        </Route>
+        {/*private routes*/}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </main>
   );
 };
 
---
+++ src/_auth/forms/SignUpForm.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+const SignUpForm = () => {
+  return (
+    <div>
+      <Button>Click Me</Button>
+    </div>
+  );
+};
+
+export default SignUpForm;
